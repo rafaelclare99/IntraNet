93e6a71f1e102e09bff2f3fe5871866c06e99d18
littleshark99 <rafael.clare@hotmail.com>
2026-01-03 17:46:10 -0300

    Criando relação agente com setor

---
+++ Models/Agente.cs
@@ -0,0 +1,12 @@
+﻿namespace IntraNet.Models
+{
+    public class Agente
+    {
+        public string AgenteId { get; set; }
+
+        public string AgenteName { get; set; }
+
+        public int SetoresId { get; set; }
+        public virtual Setores Setores { get; set; }
+    }
+}
---
+++ Models/Setores.cs
@@ -0,0 +1,13 @@
+﻿namespace IntraNet.Models
+{
+    public class Setores
+    {
+        public int SetoresId { get; set; }
+
+        public string SetoresName { get; set; }
+
+        public string SetoresDescricao { get; set; }
+
+        public List<Agente> Agentes { get; set; }
+    }
+}
