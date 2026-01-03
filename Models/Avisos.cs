85d5b7dc494817cab98c15e28da69157a51798a2
littleshark99 <rafael.clare@hotmail.com>
2026-01-03 18:25:00 -0300

    criação do quadro de avisos

---
+++ Models/Avisos.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IntraNet.Models
+{
+    public class Avisos
+    {
+        public int AvisosId { get; set; }
+
+        [Required]
+        [StringLength(120)]
+        public string Titulo { get; set; } = string.Empty;
+
+        [Required]
+        public string Conteudo { get; set; } = string.Empty;
+
+        public DateTime DataPublicacao { get; set; } = DateTime.Now;
+
+        public string Autor { get; set; } = string.Empty;
+    }
+}
